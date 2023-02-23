6c4b86f0685713c2263c29ff26d104366fe91cb4
svtom <svtomusjak@gmail.com>
2023-02-23 21:49:45 +0200

    23.02.23/10

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,14 +1,18 @@
 // src/apollo/client.js
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 import {RestLink} from 'apollo-link-rest';
+import { fetch, Headers } from "cross-fetch";
 
-if ( global.Headers === null) {
-    const fetch = require('node-fetch');
-    global.Headers = fetch.Headers
-}
+// if ( global.Headers === null) {
+//     const fetch = require('node-fetch');
+//     global.Headers = fetch.Headers
+// }
+
 
 const restLink = new RestLink({
   uri: 'https://americanbuilds.awbs.dev/wp-json/wc/v3/',
+  customFetch: fetch,
+  customHeaders: Headers,
 });
 
 const client = new ApolloClient({
